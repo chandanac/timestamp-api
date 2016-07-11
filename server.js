bb59f1eda79d5c15b5b05bd2b5088d2b06ae4aa7
Chandana <cchelamkuri@gmail.com>
2016-07-11 18:18:45 +0000

    Working solution

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 var express = require('express')
+var moment = require('moment')
 
 var app = express();
 
-app.get('/',function(req,res){
-    
-    res.send('Hello World!');
+app.get('/:query',function(req,res){
+        
+        
+        var date = req.params.query;
+        var unix = null;
+        var natural = null;
+        var dateObj = { "unix": unix, "natural": natural };
+        
+        // Check for initial unix time
+        if (+date >= 0) {
+            unix = +date;
+            natural = unixToNat(unix);
+        } 
+        
+        // Check for initial natural time
+        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+            unix = natToUnix(date);
+            natural = unixToNat(unix);
+        }        
+        
+        var dateObj = { "unix": unix, "natural": natural };
+        res.send(JSON.stringify(dateObj));
 });
 
 app.listen(8080,function(){
     
     console.log('Example app listening on 8080!');
 });
+
+
+    function natToUnix(date) {
+        // Conver from natural date to unix timestamp
+        return moment(date, "MMMM D, YYYY").format("X");
+    }
+    
+    function unixToNat(unix) {
+        // Convert unix timestamp to natural date
+        return moment.unix(unix).format("MMMM D, YYYY");
+    }
+    
